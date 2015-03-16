1f20a731d144e9119b5a9c0cee9d796f9638bedf
whiteFur Lin <whitefurlin@gmail.com>
2015-03-16 22:04:59 +0800

    Non-recuresive version completed

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -1,43 +1,45 @@
-	.syntax unified
-	.arch armv7-a
-	.text
-	.align 2
-	.thumb
-	.thumb_func
+  .syntax unified
+  .arch armv7-a
+  .text
+  .align 2
+  .thumb
+  .thumb_func
 
-	.global fibonacci
-	.type fibonacci, function
+  .global fibonacci
+  .type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+  @ ADD/MODIFY CODE BELOW
+  @ PROLOG
+  push {r3, r4, r5, r6, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+  @passed value
+  mov r3, r0
+  @previous = -1
+  mov r4, #0xFFFFFFFF
+  @result = 1
+  mov r0, #1
+  @sum = 0
+  mov r5, #0
+  @i = 0
+  mov r6, #0
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+.LOOP:
+  cmps r6, r3
+  bgt .FINISH
+  @sum = result + previous
+  add r5, r0, r4
+  @previous = result
+  mov r4, r0
+  @result = sum
+  mov r0, r5
+  add r6, r6, #1
+  bl .LOOP
+.FINISH:
+  @EPILOG
+  pop {r3, r4, r5, r6, pc}
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
 
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-	.size fibonacci, .-fibonacci
-	.end
+  .size fibonacci, .-fibonacci
+  .end
