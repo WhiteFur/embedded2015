5a56402e982456b5e81bb40c72dec21015eaeab3
whiteFur Lin <whitefurlin@gmail.com>
2015-03-16 22:43:26 +0800

    substitue tab for two spaces

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,9 +7,16 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
+  clock_t start, end;
+  double cpu_time_used;
 
   scanf("%d",&number);
+  start = clock();
   result = fibonacci(number);   
+  end = clock();
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  cpu_time_used = ((double)end - start) / CLOCKS_PER_SEC;
+  printf("time elapsed: %f sec\n", cpu_time_used);
+
 }
 
